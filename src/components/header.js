e47f85fcc130b2c0fe7bd670829f1c2270f633b0
Antony Moss <antony.moss@inviqa.com>
2020-08-06 09:21:20 +0100

    Initial commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,4 @@
+import styled from 'styled-components';
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
---
+++ src/components/styles/NavStyles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const NavStyles = styled.ul`
+  background: red;
+`;
+
+export default NavStyles;
